23871801b015ef1093ce49ce03f01b9d83ffd6bb
Jared Thomas <jjthom87@yahoo.com>
2017-11-24 16:03:08 -0500

    attempt

--- server/controller/routes.js
+++ server/controller/routes.js
@@ -57,7 +57,9 @@ router.post('/second-post', function(req,res){
 		var options = {
 		  url: `${hostUrl}/PropertyProfileOverviewServlet?boro=${num}&houseno=${req.body.house_num}&street=${req.body.street}`,
 		  headers: {
-		  	"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+		  	"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+		  	"Connection":'keep-alive'
+			"Cookie":"JSESSIONID=ADC6C972A825902FA1602C0E07D4D53D; akavpau_wr=1511557126~id=89f856c1d82586f0bca27af3339bd0e3; __utmt=1; __utma=24711658.626652818.1511328909.1511545866.1511556827.7; __utmb=24711658.2.10.1511556827; __utmc=24711658; __utmz=24711658.1511556827.7.6.utmcsr=www1.nyc.gov|utmccn=(referral)|utmcmd=referral|utmcct=/site/buildings/index.page; WT_FPC=id=d5fe4446-c0eb-4418-a991-429c50da9a60:lv=1511557267710:ss=1511556806227"
 		  }
 		};
 		var upNum = 0
@@ -67,7 +69,9 @@ router.post('/second-post', function(req,res){
 				var optionsTwo = {
 				  url: `${hostUrl}/PropertyProfileOverviewServlet?boro=${num}&houseno=${req.body.house_num}&street=${req.body.street}`,
 				  headers: {
-				  	"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+  					"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+				  	"Connection":'keep-alive'
+					"Cookie":"JSESSIONID=ADC6C972A825902FA1602C0E07D4D53D; akavpau_wr=1511557126~id=89f856c1d82586f0bca27af3339bd0e3; __utmt=1; __utma=24711658.626652818.1511328909.1511545866.1511556827.7; __utmb=24711658.2.10.1511556827; __utmc=24711658; __utmz=24711658.1511556827.7.6.utmcsr=www1.nyc.gov|utmccn=(referral)|utmcmd=referral|utmcct=/site/buildings/index.page; WT_FPC=id=d5fe4446-c0eb-4418-a991-429c50da9a60:lv=1511557267710:ss=1511556806227"
 				  }
 				};
 				request(optionsTwo, function(err, resOne, html){
@@ -85,7 +89,9 @@ router.post('/second-post', function(req,res){
 								var moreOptions = {
 								  url: `${hostUrl}/JobsQueryByLocationServlet?requestid=1&allbin=${bin_num}`,
 								  headers: {
-								  	"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+								  	"User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+								  	"Connection":'keep-alive'
+									"Cookie":"JSESSIONID=ADC6C972A825902FA1602C0E07D4D53D; akavpau_wr=1511557126~id=89f856c1d82586f0bca27af3339bd0e3; __utmt=1; __utma=24711658.626652818.1511328909.1511545866.1511556827.7; __utmb=24711658.2.10.1511556827; __utmc=24711658; __utmz=24711658.1511556827.7.6.utmcsr=www1.nyc.gov|utmccn=(referral)|utmcmd=referral|utmcct=/site/buildings/index.page; WT_FPC=id=d5fe4446-c0eb-4418-a991-429c50da9a60:lv=1511557267710:ss=1511556806227"
 								  }
 								};
 								request(moreOptions, function(error, resTwo, body){
